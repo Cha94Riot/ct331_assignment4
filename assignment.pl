5521e91ddda10932eff1275f0f7741cd38446a9b
Cha94Riot <31930027+Cha94Riot@users.noreply.github.com>
2017-11-15 21:21:41 +0000

    Assignment4
    Completed Assignment 4

--- assignment.pl
+++ assignment.pl
@@ -1,7 +1,23 @@
 %Definitions for isElementInList(El, List)
+isElementInList(el,[]).
+isElementInList(H,[H|[]]):- !.
+isElementInList(El,[H|T]):-
+    El=H, !;
+    isElementInList(El,T).
 
 %Definitions for reverseList(List, ReversedList)
+reverseList([],[]).
+reverseList([H|T], RevList):-
+    reverseList(T, Rev),
+    mergeLists(Rev,[H],RevList).
 
 %insertElementIntoListEnd(El, List, NewList)
+insertElementIntoListEnd([],L,L).
+insertElementIntoListEnd(El,L,Output):-
+    mergeLists(L,[El],Output).
 
 %Definitions for mergeLists(List1, List2, Merged)
+mergeLists([],L,L).
+mergeLists([H|T],L,[H|M]):-
+    mergeLists(T,L,M).
+
